90d56d58a863812d9417cc89449c039364ce1460
Khemra <saidsugarfree@yahoo.com>
2023-10-06 16:27:04 -0500

    imdecode cv2.imread_color

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def preprocessing(file):
     in_memory_file = io.BytesIO()
     file.save(in_memory_file)
     data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
-    img = cv2.imdecode(data, 0)
+    img = cv2.imdecode(data, cv2.IMREAD_COLOR) 
     res = cv2.resize(img, dsize=(28, 28), interpolation=cv2.INTER_CUBIC)
     # file.save("static/UPLOAD/img.png") # saving uploaded img
     # cv2.imwrite("static/UPLOAD/test.png", res) # saving processed image
